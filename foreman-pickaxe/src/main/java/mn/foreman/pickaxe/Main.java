c10c18dee63a46b9599c42c48def6a1a1e9545b6
Dan Lawrence <delawr0190@gmail.com>
2017-12-15 19:53:01 -0500

    Pickaxe build process

--- foreman-pickaxe/src/main/java/mn/foreman/pickaxe/Main.java
+++ foreman-pickaxe/src/main/java/mn/foreman/pickaxe/Main.java
@@ -5,17 +5,17 @@ import mn.foreman.pickaxe.configuration.ConfigurationFactory;
 import mn.foreman.pickaxe.configuration.yml.YmlConfigurationFactory;
 import mn.foreman.pickaxe.run.RunMe;
 
-import org.apache.commons.cli.*;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /** The application entry point. */
 public class Main {
-
-    /** The config file option. */
-    private static final String CONFIG_OPTION = "c";
 
     /** The logger for this class. */
     private static final Logger LOG =
@@ -27,33 +27,37 @@ public class Main {
      * @param args The command line arguments.
      */
     public static void main(final String[] args) {
-        final CommandLineParser parser = new DefaultParser();
+        final OptionParser optionParser = new OptionParser();
 
-        final Options options = new Options();
-        options.addOption(CONFIG_OPTION, true, "Set config file path");
+        // Help
+        optionParser
+                .acceptsAll(Arrays.asList("h", "?"), "Shows help")
+                .forHelp();
+
+        // Config file
+        final OptionSpec<String> configSpec =
+                optionParser
+                        .acceptsAll(
+                                Arrays.asList("c", "config"),
+                                "Sets the configuration file")
+                        .withRequiredArg()
+                        .ofType(String.class)
+                        .required();
+
+        final OptionSet optionSet = optionParser.parse(args);
 
         try {
-            final CommandLine commandLine = parser.parse(options, args);
+            final String configFile = optionSet.valueOf(configSpec);
 
-            if (commandLine.hasOption(CONFIG_OPTION)) {
-                final String config =
-                        commandLine.getOptionValue(CONFIG_OPTION).trim();
-                if (!config.isEmpty()) {
-                    final ConfigurationFactory configurationFactory =
-                            new YmlConfigurationFactory();
-                    final Configuration configuration =
-                            configurationFactory.load(config);
+            final ConfigurationFactory configurationFactory =
+                    new YmlConfigurationFactory();
+            final Configuration configuration =
+                    configurationFactory.load(configFile);
 
-                    final RunMe runMe = new RunMe(configuration);
-                    runMe.run();
-                } else {
-                    LOG.error("Missing config file!");
-                }
-            } else {
-                LOG.error("Missing config file!");
-            }
-        } catch (final ParseException | IOException e) {
-            LOG.error("Exception occurred while starting", e);
+            final RunMe runMe = new RunMe(configuration);
+            runMe.run();
+        } catch (final IOException ioe) {
+            LOG.error("Missing config file!");
         }
     }
 }
