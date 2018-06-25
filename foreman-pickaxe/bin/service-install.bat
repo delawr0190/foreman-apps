74bb1900accc84df751b1929720f5b0178e568d1
Dan Lawrence <delawr0190@gmail.com>
2018-06-24 21:15:46 -0400

    Adding comments to dev props.  Also add Windows service scripts

---
+++ foreman-pickaxe/bin/service-install.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+set PICKAXE_HOME=%~dp0..
+
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+    set NSSM="%PICKAXE_HOME%\bin\support\win64\nssm.exe"
+) else (
+    set NSSM="%PICKAXE_HOME%\bin\support\win32\nssm.exe"
+)
+
+%NSSM% install Pickaxe "%PICKAXE_HOME%\bin\pickaxe.bat" ""
+%NSSM% set Pickaxe Description "Foreman Pickaxe"
---
+++ foreman-pickaxe/bin/service-stop.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+set PICKAXE_HOME=%~dp0..
+
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+    set NSSM="%PICKAXE_HOME%\bin\support\win64\nssm.exe"
+) else (
+    set NSSM="%PICKAXE_HOME%\bin\support\win32\nssm.exe"
+)
+
+%NSSM% stop Pickaxe
