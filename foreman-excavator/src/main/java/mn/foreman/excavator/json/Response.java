908a96c8ef53ae7db7ffdbee91cf4ddb2933a5d5
Dan Lawrence <delawr0190@gmail.com>
2018-07-02 01:25:20 -0400

    Missing javadoc before I forget again

--- foreman-excavator/src/main/java/mn/foreman/excavator/json/Response.java
+++ foreman-excavator/src/main/java/mn/foreman/excavator/json/Response.java
@@ -1,5 +1,9 @@
 package mn.foreman.excavator.json;
 
+/**
+ * A {@link Response} provides an aggregator interface on all POJOs that
+ * represent a deserialized excavator response.
+ */
 public interface Response {
 
 }
