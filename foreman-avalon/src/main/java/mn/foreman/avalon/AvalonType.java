f7bc67ed9a12076d61f64eeb627674bfa45478fc
Dan Lawrence <delawr0190@gmail.com>
2020-04-29 09:02:35 -0400

    Adding additional avalon 10xx models

--- foreman-avalon/src/main/java/mn/foreman/avalon/AvalonType.java
+++ foreman-avalon/src/main/java/mn/foreman/avalon/AvalonType.java
@@ -13,26 +13,32 @@ import java.util.concurrent.ConcurrentHashMap;
 public enum AvalonType
         implements MinerType {
 
-    /** The Avalon 741. */
+    /** Avalon 741. */
     AVALON_741("Ver[741", "avalonminer-741"),
 
-    /** The Avalon 842. */
+    /** Avalon 842. */
     AVALON_821("Ver[821", "avalonminer-821"),
 
-    /** The Avalon 841. */
+    /** Avalon 841. */
     AVALON_841("Ver[841", "avalonminer-841"),
 
-    /** The Avalon 851. */
+    /** Avalon 851. */
     AVALON_851("Ver[851", "avalonminer-851"),
 
-    /** The Avalon 911. */
+    /** Avalon 911. */
     AVALON_911("Ver[911", "avalonminer-911"),
 
-    /** The Avalon 921. */
+    /** Avalon 921. */
     AVALON_921("Ver[921", "avalonminer-921"),
 
-    /** The Avalon 1047. */
-    AVALON_1047("Ver[1047", "avalonminer-1047");
+    /** Avalon 1026. */
+    AVALON_1026("Ver[1026", "avalonminer-1026"),
+
+    /** Avalon 1047. */
+    AVALON_1047("Ver[1047", "avalonminer-1047"),
+
+    /** Avalon 1066. */
+    AVALON_1066("Ver[1066", "avalonminer-1066");
 
     /** All of the types, by string, mapped to their type. */
     private static final Map<String, AvalonType> TYPE_MAP =
